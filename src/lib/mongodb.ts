8865afd05e303c794a592bd98d6118e4b57e7282
deepakgs536 <deepakgs536@gmail.com>
2026-02-19 20:25:03 +0530

    error handling in DB connection

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -2,10 +2,6 @@ import mongoose from 'mongoose';
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
-if (!MONGODB_URI) {
-    throw new Error('Please define the MONGODB_URI environment variable in .env');
-}
-
 interface MongooseCache {
     conn: typeof mongoose | null;
     promise: Promise<typeof mongoose> | null;
@@ -27,11 +23,27 @@ async function connectDB() {
         return cached.conn;
     }
 
-    if (!cached.promise) {
-        cached.promise = mongoose.connect(MONGODB_URI!).then((m) => m);
+    if (!MONGODB_URI) {
+        throw new Error('Please define the MONGODB_URI environment variable in .env');
     }
 
-    cached.conn = await cached.promise;
+    if (!cached.promise) {
+        const opts = {
+            bufferCommands: false,
+        };
+
+        cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
+            return mongoose;
+        });
+    }
+
+    try {
+        cached.conn = await cached.promise;
+    } catch (e) {
+        cached.promise = null;
+        throw e;
+    }
+
     return cached.conn;
 }
 
